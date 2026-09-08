c9b5ecf40d1910b2b3d8e30481b533ed9084de3b
Rob Sargant <robert@sargant.com>
2026-09-08 19:40:38 +0100

    Add OpenTofu Proxmox provider

---
+++ infra/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.111.1"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://192.168.37.11:8006/"
+  insecure = true
+}
