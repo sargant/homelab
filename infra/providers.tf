6a0ed8f5b75d7007321cdf791eb67116e1b12699
Rob Sargant <robert@sargant.com>
2026-09-08 22:24:20 +0100

    Add UniFi provider

--- infra/providers.tf
+++ infra/providers.tf
@@ -4,6 +4,11 @@ terraform {
       source  = "bpg/proxmox"
       version = "0.111.1"
     }
+
+    unifi = {
+      source  = "ubiquiti-community/unifi"
+      version = "0.55.0"
+    }
   }
 }
 
@@ -11,3 +16,5 @@ provider "proxmox" {
   endpoint = "https://127.0.0.1:8006/"
   insecure = true
 }
+
+provider "unifi" {}
