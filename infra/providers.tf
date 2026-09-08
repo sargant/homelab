33107169138451f6136b808678f5c90d3c3190e3
Rob Sargant <robert@sargant.com>
2026-09-08 23:33:06 +0100

    Define Tailscale LXC in OpenTofu (#6)
    * Configure Proxmox OpenTofu provider
    
    * Define Tailscale LXC in OpenTofu
    
    * Ignore OpenTofu working state
    
    * Use SLAAC for Tailscale IPv6
    
    * Add UniFi provider
    
    * Manage Tailscale DNS record
    
    * Keep Tailscale DNS with service definition
    
    * Remove separate DNS resource file
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Give Tailscale its own local MAC
    
    * Use a random locally administered MAC for Tailscale
    
    * Use a random locally administered MAC for Tailscale
    
    * Use localhost for Proxmox API
    
    * Manage Debian LXC template
    
    * Use managed Debian LXC template

---
+++ infra/providers.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "bpg/proxmox"
+      version = "0.111.1"
+    }
+
+    unifi = {
+      source  = "ubiquiti-community/unifi"
+      version = "0.55.0"
+    }
+  }
+}
+
+provider "proxmox" {
+  endpoint = "https://localhost:8006/"
+  insecure = true
+}
+
+provider "unifi" {}
---
+++ infra/tailscale.tf
@@ -0,0 +1,61 @@
+resource "proxmox_virtual_environment_container" "tailscale" {
+  node_name = "vm-host"
+  vm_id     = 1021
+
+  architecture = "amd64"
+  cores        = 1
+
+  memory {
+    dedicated = 256
+    swap      = 256
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+
+  features {
+    nesting = true
+  }
+
+  device_passthrough {
+    path = "/dev/net/tun"
+  }
+
+  initialization {
+    hostname = "tailscale"
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+
+      ipv6 {
+        address = "auto"
+      }
+    }
+  }
+
+  network_interface {
+    name        = "eth0"
+    bridge      = "vmbr0"
+    firewall    = true
+    mac_address = "02:EE:88:40:BC:33"
+  }
+
+  operating_system {
+    template_file_id = proxmox_download_file.debian_13.id
+    type             = "debian"
+  }
+
+  start_on_boot = true
+  started       = true
+  unprivileged  = true
+}
+
+resource "unifi_dns_record" "tailscale" {
+  name        = "tailscale.home.arpa"
+  record_type = "A"
+  value       = "192.168.37.21"
+}
