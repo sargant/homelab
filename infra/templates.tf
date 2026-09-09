9720ad6edeffc164e9064390d5b6a2a9e2eb213c
Rob Sargant <robert@sargant.com>
2026-09-09 13:29:03 +0100

    Allow OpenTofu to overwrite unmanaged template

--- infra/templates.tf
+++ infra/templates.tf
@@ -1,6 +1,7 @@
 resource "proxmox_download_file" "debian_13" {
-  content_type = "vztmpl"
-  datastore_id = "local"
-  node_name    = "vm-host"
-  url          = "http://download.proxmox.com/images/system/debian-13-standard_13.6-1_amd64.tar.zst"
+  content_type        = "vztmpl"
+  datastore_id        = "local"
+  node_name           = "vm-host"
+  url                 = "http://download.proxmox.com/images/system/debian-13-standard_13.6-1_amd64.tar.zst"
+  overwrite_unmanaged = true
 }
