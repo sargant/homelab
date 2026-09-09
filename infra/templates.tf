0e979156cdb86bca750131fa2d8592ec83ac4cee
Rob Sargant <robert@sargant.com>
2026-09-09 13:25:51 +0100

    Use HTTP for Proxmox template download

--- infra/templates.tf
+++ infra/templates.tf
@@ -2,5 +2,5 @@ resource "proxmox_download_file" "debian_13" {
   content_type = "vztmpl"
   datastore_id = "local"
   node_name    = "vm-host"
-  url          = "https://download.proxmox.com/images/system/debian-13-standard_13.6-1_amd64.tar.zst"
+  url          = "http://download.proxmox.com/images/system/debian-13-standard_13.6-1_amd64.tar.zst"
 }
