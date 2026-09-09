4aa0279d3e4b189ca8aebeb756ae7abf49823c43
Rob Sargant <robert@sargant.com>
2026-09-09 21:31:31 +0100

    Add print server LXC definition

---
+++ infra/print-server.tf
@@ -0,0 +1,59 @@
+resource "proxmox_virtual_environment_container" "print_server" {
+  node_name = "vm-host"
+  vm_id     = 101
+
+  cpu {
+    architecture = "amd64"
+    cores        = 1
+  }
+
+  memory {
+    dedicated = 256
+    swap      = 256
+  }
+
+  disk {
+    datastore_id = "local-lvm"
+    size         = 8
+  }
+
+  features {
+    nesting = true
+  }
+
+  initialization {
+    hostname = "print-server"
+
+    user_account {
+      keys = [
+        trimspace(file("/root/.ssh/ansible-bootstrap.pub"))
+      ]
+    }
+
+    ip_config {
+      ipv4 {
+        address = "dhcp"
+      }
+
+      ipv6 {
+        address = "dhcp"
+      }
+    }
+  }
+
+  network_interface {
+    name        = "eth0"
+    bridge      = "vmbr0"
+    firewall    = true
+    mac_address = "02:7A:41:C3:8D:52"
+  }
+
+  operating_system {
+    template_file_id = proxmox_download_file.debian_13.id
+    type             = "debian"
+  }
+
+  start_on_boot = true
+  started       = true
+  unprivileged  = true
+}
