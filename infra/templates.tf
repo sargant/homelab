5e90646f6f8cf1ee70ae299903d26a2ff3217d9e
Rob Sargant <robert@sargant.com>
2026-09-10 17:14:52 +0100

    Fold Debian cloud image into templates

--- infra/templates.tf
+++ infra/templates.tf
@@ -7,3 +7,12 @@ resource "proxmox_download_file" "debian_13" {
   checksum_algorithm  = "sha512"
   checksum            = "4c0c27ca6ceab5ef0b84db57825a00f26157ef1854bafe97297813e1cbe8ecb8cc9c453cab6b3b0efe1ba193a50c47ece1e41d950e411b8730b835b71e9e754b"
 }
+
+resource "proxmox_download_file" "debian_13_cloud" {
+  content_type = "import"
+  datastore_id = "local"
+  node_name    = "vm-host"
+  url          = "https://cloud.debian.org/images/cloud/trixie/latest/debian-13-generic-amd64.qcow2"
+  file_name    = "debian-13-generic-amd64.qcow2"
+  overwrite    = false
+}
