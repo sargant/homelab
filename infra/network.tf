cf73692dfd8dfbe820470d227fba57bea553ae15
Rob Sargant <robert@sargant.com>
2026-09-11 13:54:10 +0100

    Add managed hostnames to host metadata

--- infra/network.tf
+++ infra/network.tf
@@ -1,21 +1,24 @@
 locals {
   hosts = {
     tailscale = {
-      mac = "02:EE:88:40:BC:33"
-      ip  = "192.168.37.21"
-      dns = "tailscale.home.arpa"
+      hostname = "tailscale"
+      mac      = "02:EE:88:40:BC:33"
+      ip       = "192.168.37.21"
+      dns      = "tailscale.home.arpa"
     }
 
     print_server = {
-      mac = "02:7A:41:C3:8D:52"
-      ip  = "192.168.37.41"
-      dns = "print-server.home.arpa"
+      hostname = "print-server"
+      mac      = "02:7A:41:C3:8D:52"
+      ip       = "192.168.37.41"
+      dns      = "print-server.home.arpa"
     }
 
     paperless = {
-      mac = "02:9D:44:7C:A1:B6"
-      ip  = "192.168.37.44"
-      dns = "paperless.home.arpa"
+      hostname = "paperless"
+      mac      = "02:9D:44:7C:A1:B6"
+      ip       = "192.168.37.44"
+      dns      = "paperless.home.arpa"
     }
   }
 }
