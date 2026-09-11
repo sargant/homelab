484b8185ce4ff6eca459c0e60bb0f6de3eef53bf
Rob Sargant <robert@sargant.com>
2026-09-11 14:06:43 +0100

    Move Paperless test VM to temporary network identity

--- infra/network.tf
+++ infra/network.tf
@@ -17,8 +17,8 @@ locals {
     paperless = {
       hostname = "paperless"
       mac      = "02:9D:44:7C:A1:B6"
-      ip       = "192.168.37.44"
-      dns      = "paperless.home.arpa"
+      ip       = "192.168.37.99"
+      dns      = "paperless-test.home.arpa"
     }
   }
 }
