d0614e02aeb47fb9236276f4e964bbc7a567f017
Rob Sargant <robert@sargant.com>
2026-09-14 13:35:40 +0100

    Declare print server LXC sizing

--- infra/print-server.tf
+++ infra/print-server.tf
@@ -5,4 +5,9 @@ module "print_server" {
   display_name     = "Print server"
   vm_id            = 1041
   template_file_id = proxmox_download_file.debian_13.id
+
+  cores     = 1
+  memory    = 256
+  swap      = 256
+  disk_size = 8
 }
